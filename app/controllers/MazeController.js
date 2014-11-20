a6b90e32913f204e00806b8baac1a8fd426fafda
Steve Liew <ys.steve@gmail.com>
2014-11-20 17:54:17 +1300

    updated key binds

--- app/controllers/MazeController.js
+++ app/controllers/MazeController.js
@@ -1,4 +1,11 @@
-function MazeController() {
+var KEY = {
+    LEFT:  37,
+    RIGHT: 39,
+    UP:    38,
+    DOWN:  40
+}
+
+var MazeController = function() {
   this.view  = new MazeView()
   this.model = new Maze()
 }
@@ -8,11 +15,29 @@ MazeController.prototype = {
     this.bindListeners()
   },
   bindListeners: function() {
-    .addEventListener('click', this.movePlayer.bind(this))
+    document.addEventListener('keyup', this.keyFunction.bind(this))
   },
-  movePlayer: function() {
-    this.model.moveCount()
-    var newCount = this.model.moves
-    this.view.showCount(newCount)
+  keyFunction: function(event) {
+    switch (event.keyCode) {
+        case KEY.LEFT:
+            this.view.moveLeft();
+            this.model.moveCount();
+            break;
+        case KEY.RIGHT:
+            this.view.moveRight();
+            this.model.moveCount();
+            break;
+        case KEY.UP:
+            this.view.moveLeft();
+            this.model.moveCount();
+            break;
+        case KEY.DOWN:
+            this.view.moveDown();
+            this.model.moveCount();
+            break;
+
+        default:
+            return;
+    }
   }
 };
