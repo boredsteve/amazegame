6e70073c37b2e86f0d0b833ee8a56019bf465ad2
Rob Edgecombe <rob.edgecombe@gmail.com>
2014-11-21 15:11:29 +1300

    second player added

--- app/controllers/MazeController.js
+++ app/controllers/MazeController.js
@@ -1,15 +1,19 @@
 var KEY = {
-    LEFT:  37,
-    RIGHT: 39,
-    UP:    38,
-    DOWN:  40
+    P1_LEFT:  37,
+    P1_RIGHT: 39,
+    P1_UP:    38,
+    P1_DOWN:  40,
+    P2_LEFT:  65,
+    P2_RIGHT: 68,
+    P2_UP:    87,
+    P2_DOWN:  83
 }
 
 var MazeController = function() {
   this.view  = new MazeView()
   this.restartCtrl = new RestartController()
-  this.model = this.restartCtrl.getMaze()
-  // this.model = new Maze()
+  // this.model = this.restartCtrl.getMaze()
+  this.model = new Maze()
 }
 
 MazeController.prototype = {
@@ -27,30 +31,55 @@ MazeController.prototype = {
 
     switch (event.keyCode) {
 
-        case KEY.LEFT:
+        case KEY.P1_LEFT:
              this.view.moveLeft();
              this.model.moveCount();
             // console.log("left key pressed")
             // console.log("number of moves " + this.model.moves);
             break;
-        case KEY.RIGHT:
+        case KEY.P1_RIGHT:
              this.view.moveRight();
              this.model.moveCount();
              // console.log("right key pressed")
              // console.log("number of moves " + this.model.moves);
             break;
-        case KEY.UP:
+        case KEY.P1_UP:
              this.view.moveUp();
              this.model.moveCount();
              // console.log("up key pressed")
              // console.log("number of moves " + this.model.moves);
             break;
-        case KEY.DOWN:
+        case KEY.P1_DOWN:
             this.view.moveDown();
              this.model.moveCount();
              // console.log("down key pressed")
              // console.log("number of moves " + this.model.moves);
             break;
+        case KEY.P2_LEFT:
+             this.view.moveLeft2();
+             this.model.moveCount2();
+            // console.log("left key pressed")
+            // console.log("number of moves " + this.model.moves);
+            break;
+        case KEY.P2_RIGHT:
+             this.view.moveRight2();
+             this.model.moveCount2();
+             // console.log("right key pressed")
+             // console.log("number of moves " + this.model.moves);
+            break;
+        case KEY.P2_UP:
+             this.view.moveUp2();
+             this.model.moveCount2();
+             // console.log("up key pressed")
+             // console.log("number of moves " + this.model.moves);
+            break;
+        case KEY.P2_DOWN:
+            this.view.moveDown2();
+             this.model.moveCount2();
+             // console.log("down key pressed")
+             // console.log("number of moves " + this.model.moves);
+            break;
+
 
         default:
             return;
--- app/models/Maze.js
+++ app/models/Maze.js
@@ -1,11 +1,20 @@
 function Maze() {
   this.moves = 0
+  this.moves2 = 0
 }
 
 Maze.prototype = {
   moveCount: function() {
     this.moves += 1
     console.log("moves is " + this.moves)
-
+  },
+  moveCount2: function() {
+    this.moves2 += 1
+    console.log("moves2 is " + this.moves)
+  },
+  resetMoves: function() {
+    this.moves = 0
+    this.moves2 = 0
+    console.log("moves after reset " + this.moves)
   }
 }
